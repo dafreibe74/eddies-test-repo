b2007edd2d011ebc8ae8eaabc58d63a974b35681
ferrjess <125903996+ferrjess@users.noreply.github.com>
2023-04-12 19:10:08 -0500

    Update data_exploration.py

--- data_exploration.py
+++ data_exploration.py
@@ -3,4 +3,4 @@ print("I really hate using visual studio code")
 =======
 print ("We could just do this directly in Github")
 >>>>>>> 3e7cf816cd6a005b84c047f8ee8d2afe2a7b4004
-print("Can someone tell me how to connect GitHub desktop to my jupyter lab so I don't have to use visual studio code? Please and thank you!")
+print("ugh")
